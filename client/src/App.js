9ed2e19899eb73fdd29773ea08cee4d671b41ca3
Nicholas Rynearson <nrynearson@gmail.com>
2021-08-21 20:04:43 -0400

    sets up Routes in App; sets up skeleton for Home screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-
+import { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom"
+import Home from "./screens/Home/Home";
 import './App.css';
 
 function App() {
+  const [admin, setAdmin] = useState()
+
   return (
     <div className="App">
-
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/login">
+          {/* <Login setAdmin={setAdmin} /> */}
+        </Route>
+        <Route exact path="/update">
+          {/* <UpdatePortfolio admin={admin} setAdmin={setAdmin} /> */}
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,38 @@
+
+
+function Home() {
+  return(
+    <div className="home">
+      <div className="header">
+        <div className="left-panel">left</div>
+        <div className="divider">div</div>
+        <div className="right-panel">right</div>
+      </div>
+      <div className="projects-tab">
+        <div className="left-panel">left</div>
+        <div className="divider">div</div>
+        <div className="right-panel">projects</div>
+      </div>
+      <div className="projects-menu"></div>
+      <div className="about-tab">
+        <div className="left-panel">left</div>
+        <div className="divider">div</div>
+        <div className="right-panel">about</div>
+      </div>
+      <div className="about"></div>
+      <div className="links-tab">
+        <div className="left-panel">left</div>
+        <div className="divider">div</div>
+        <div className="right-panel">links</div>
+      </div>
+      <div className="links"></div>
+      <div className="footer">
+        <div className="left-panel">left</div>
+        <div className="divider">div</div>
+        <div className="right-panel">right</div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
